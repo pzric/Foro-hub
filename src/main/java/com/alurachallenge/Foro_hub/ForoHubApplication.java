34060bab8194aa45a5445bc356a2fc0c34df2e58
pzric <pzrico@hotmail.com>
2024-07-04 17:59:44 -0600

    commit inicial

---
+++ src/main/java/com/alurachallenge/Foro_hub/ForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.Foro_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alurachallenge/Foro_hub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.Foro_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
